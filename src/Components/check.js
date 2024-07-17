a3091fcf26ff4f6ea9507d75aff9fd8cdafd891e
Soumik Majumder <soumikcode02@gmail.com>
2024-07-17 23:25:36 +0530

    simplified logic

--- src/Components/check.js
+++ src/Components/check.js
@@ -4,27 +4,19 @@ function isWinner(board, symbol, winner)
         return "-";
     }
 
-    if (board[0] == board[1] && board[1] == board[2] && board[2] == symbol) {
-        return symbol;
-    }
-    if (board[3] == board[4] && board[4] == board[5] && board[5] == symbol) {
-        return symbol;
-    }
-    if (board[6] == board[7] && board[7] == board[8] && board[8] == symbol) {
-        return symbol;
+    for (let i = 0; i < 7; i += 3)
+    {
+        if (board[i] == board[i + 1] && board[i + 1] == board[i + 2] && board[i + 2] == symbol) {
+            return symbol;
+        }
     }
 
-
-    if (board[0] == board[3] && board[3] == board[6] && board[6] == symbol) {
-        return symbol;
+    for (let i = 0; i < 3; i++)
+    {
+        if (board[i] == board[i + 3] && board[i + 3] == board[i + 6] && board[i + 6] == symbol) {
+            return symbol;
+        }
     }
-    if (board[1] == board[4] && board[4] == board[7] && board[7] == symbol) {
-        return symbol;
-    }
-    if (board[2] == board[5] && board[5] == board[8] && board[8] == symbol) {
-        return symbol;
-    }
-
 
     if (board[0] == board[4] && board[4] == board[8] && board[8] == symbol) {
         return symbol;
