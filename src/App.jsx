4006851b48c1e344d7708754d3fb249ad2f83c63
Soumik Majumder <soumikcode02@gmail.com>
2024-12-12 18:09:03 +0530

    na

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useEffect } from "react"
 import Grid from "./Components/Grid"
 
 function App() {
+
+  async function apiCall()
+  {
+    try {
+      await fetch("https://lets-chat-fjae.onrender.com/api/check");
+    }
+    catch {
+      console.log("nothing");
+    }
+  }
+
+  useEffect(() => {
+    apiCall();
+  }, [])
 
   return (
     <div className="flex items-center justify-center h-[100vh] bg-[#393939]">
